e88867e99e7aef501130f49c59654c2e73db94d2
Hakan Kahraman <hakank4hraman@gmail.com>
2025-12-17 23:21:54 +0300

    Add Build a sentence maker

---
+++ JavaScript/Build a Sentence Maker/script.js
@@ -0,0 +1,23 @@
+let adjective = "brave";
+let noun = "knight";
+let verb = "angry";
+let place = "castle";
+let adjective2 = "smoking";
+let noun2 = "marshmallows";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory)
+
+adjective = "mysterious";
+noun = "alien";
+verb = "excited";
+place = "spaceship";
+adjective2 = "glowing";
+noun2 = "batteries";
+
+//  İkinci hikaye değişkenini oluşturuyoruz
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+//  İkinci hikayeyi konsola yazdırıyoruz
+console.log("Second story: " + secondStory);
