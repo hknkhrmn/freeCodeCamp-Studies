61e1e0b195edbe22adb8986ff5fb90227b720fa9
Hakan Kahraman <hakank4hraman@gmail.com>
2026-01-24 21:10:59 +0300

    update

---
+++ JavaScript/Build a Fortune Teller/app.js
@@ -0,0 +1,27 @@
+// 1. Define fortune messages
+const fortune1 = "Your cat will look very cuddly today.";
+const fortune2 = "The weather will be nice tomorrow.";
+const fortune3 = "Be cautious of your new neighbors.";
+const fortune4 = "You will find a new hobby soon.";
+const fortune5 = "It would be wise to avoid the color red today.";
+
+// 2. Generate a random number between 1 and 5 (inclusive)
+const randomNumber = Math.floor(Math.random() * 5) + 1;
+
+// 3. Select the fortune based on the random number
+let selectedFortune;
+
+if (randomNumber === 1) {
+  selectedFortune = fortune1;
+} else if (randomNumber === 2) {
+  selectedFortune = fortune2;
+} else if (randomNumber === 3) {
+  selectedFortune = fortune3;
+} else if (randomNumber === 4) {
+  selectedFortune = fortune4;
+} else if (randomNumber === 5) {
+  selectedFortune = fortune5;
+}
+
+// 4. Log the selected fortune to the console
+console.log(selectedFortune);
