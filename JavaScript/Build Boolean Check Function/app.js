8519191dd07222e7787e137f69d44b2b244ec81c
Hakan Kahraman <hakank4hraman@gmail.com>
2026-01-27 20:39:35 +0300

    add boolean function

---
+++ JavaScript/Build Boolean Check Function/app.js
@@ -0,0 +1,15 @@
+function booWho(value) {
+  return typeof value === 'boolean';
+}
+
+// Test cases
+console.log(booWho(true));      
+console.log(booWho(false));     
+console.log(booWho([1, 2, 3]));  
+console.log(booWho([].slice));   
+console.log(booWho({ "a": 1 })); 
+console.log(booWho(1));          
+console.log(booWho(NaN));        
+console.log(booWho("a"));        
+console.log(booWho("true"));     
+console.log(booWho("false"));    
