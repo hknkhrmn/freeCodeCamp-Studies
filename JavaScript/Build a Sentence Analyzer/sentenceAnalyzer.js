506653d4d8a267cc51dfe50aea373b8cb4bb142c
Hakan Kahraman <hakank4hraman@gmail.com>
2026-03-03 15:17:51 +0300

    Refactor console log statements and add comments for clarity in sentence analyzer functions

--- JavaScript/Build a Sentence Analyzer/sentenceAnalyzer.js
+++ JavaScript/Build a Sentence Analyzer/sentenceAnalyzer.js
@@ -1,3 +1,4 @@
+// Sesli harfleri bulması için bir fonksiyon oluşturduk
 function getVowelCount(sentence) {
   const vowels = "aeiou";
   let count = 0;
@@ -10,9 +11,11 @@ function getVowelCount(sentence) {
   return count;
 }
 
+// Cümlede ki sesli harfi buluyor mu diye consol.log ile çıktısını aldık
 const vowelCount = getVowelCount("Apples are tasty fruits");
-console.log(`Vowel Count: ${vowelCount}`);
+console.log(`Vowel Count: ${vowelCount}`); // Vowel Count: 7
 
+//Sessiz harfleri bulması için bir fonksiyon oluşturduk
 function getConsonantCount(sentence) {
   const consonants = "bcdfghjklmnpqrstvwxyz";
   let count = 0;
@@ -25,9 +28,11 @@ function getConsonantCount(sentence) {
   return count;
 }
 
+// Aynı şekilde buluyormu diye çıktı aldık
 const consonantCount = getConsonantCount("Coding is fun");
-console.log(`Consonant Count: ${consonantCount}`);
+console.log(`Consonant Count: ${consonantCount}`); // Consonant Count: 7
 
+// Noktalama işaretleri için yaptık
 function getPunctuationCount(sentence) {
   const punctuations = ".,!?;:-()[]{}\"'–";
   let count = 0;
@@ -40,14 +45,16 @@ function getPunctuationCount(sentence) {
   return count;
 }
 
+// Oluyormu diye çıktı aldık.
 const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
-console.log(`Punctuation Count: ${punctuationCount}`);
+console.log(`Punctuation Count: ${punctuationCount}`); // Punctuation Count: 9
 
 function getWordCount(sentence) {
   if (sentence.trim() === '') {
     return 0;
   }
   
+  // Komple proje için yaptık 
   const words = sentence.trim().split(' ');
   let count = 0;
 
@@ -61,4 +68,8 @@ function getWordCount(sentence) {
 }
 
 const wordCount = getWordCount("I love freeCodeCamp");
-console.log(`Word Count: ${wordCount}`);
+console.log(`Word Count: ${wordCount}`); // Word Count: 3
+
+
+
+
