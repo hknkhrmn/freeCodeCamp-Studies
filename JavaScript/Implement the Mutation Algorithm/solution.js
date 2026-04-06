c2975a5c71a24aa1d0fb1ac8b3e9f9f7b87293cd
Hakan Kahraman <hakank4hraman@gmail.com>
2026-04-06 22:22:02 +0300

    Add: Implement the Mutation Algorithm

---
+++ JavaScript/Implement the Mutation Algorithm/solution.js
@@ -0,0 +1,16 @@
+function mutation(arr) {
+  const hedef = arr[0].toLowerCase();
+  const aranan = arr[1].toLowerCase();
+
+
+  for (let harf of aranan) {
+    if (!hedef.includes(harf)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+console.log (mutation(["hello", "hey"]));  // false
+console.log (mutation(["Hakan", "hakhak"])); // true
